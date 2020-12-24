46a75987b5fa5c3d37b84216fed18bd3dd84abe1
Jordan Enskat <jordanenskat@Jordans-MacBook-Air.local>
2020-12-24 11:03:15 -0600

    react app first publish

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Router>
+     <Navbar />
+     <Switch>
+       <Router path='./' exact />
+     </Switch>
+     </Router>
+    </>
+  );
+}
+
+export default App;
